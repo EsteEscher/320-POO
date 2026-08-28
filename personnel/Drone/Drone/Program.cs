9382f9272e5be27e4082af94b3cd8b7790f04060
esteban-escher <pi53hgb@eduvaud.ch>
2026-08-28 11:26:44 +0200

    Jetpack cat

---
+++ personnel/Drone/Drone/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Drone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Premier clear et appariton de départ drone
+            Console.Clear();
+            Console.Write("x-0-x");
+
+            //Variable
+            int posX = 0;
+            const int posY = 10;
+            int batterie = 50;
+
+            //Fonction pour le faire avancer
+            while (batterie >= 2)
+            {
+                Console.Clear();
+                batterie = batterie - 2;
+                posX++;
+                Console.SetCursorPosition(posX, posY);
+                Console.Write("x-0-x");
+                Thread.Sleep(150);
+            }
+
+            //Last position + death
+            Console.Clear() ;
+            Console.SetCursorPosition(posX, posY);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("x---x");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Read();
+
+        }
+    }
+}
