164612df7da4097abdc30f5ff50428d04cdadd7e
esteban-escher <pi53hgb@eduvaud.ch>
2026-08-28 12:11:26 +0200

    Jetpack cat ver 2

--- personnel/Drone/Drone/Program.cs
+++ personnel/Drone/Drone/Program.cs
@@ -17,28 +17,47 @@ namespace Drone
 
             //Variable
             int posX = 0;
-            const int posY = 10;
+            int posY = 10;
+            int posX1 = 5;
+            int posY1 = 5;
             int batterie = 50;
+            int batterie2 = 50;
 
-            //Fonction pour le faire avancer
+            //Boucle pour le faire avancer
             while (batterie >= 2)
             {
-                Console.Clear();
-                batterie = batterie - 2;
                 posX++;
-                Console.SetCursorPosition(posX, posY);
-                Console.Write("x-0-x");
+                posX1++;
+                batterie = battLoss(batterie);
+                drawDrone(posX, posY);
+                drawDrone(posX1, posY1);
                 Thread.Sleep(150);
+                Console.Clear();
             }
+            Console.Clear();
+            death(posX, posY);
+            death(posX1, posY1);
+            Console.Read();
 
+        }
+        static void drawDrone(int posX, int posY)
+        {
+            Console.SetCursorPosition(posX, posY);
+            Console.Write("x-0-x");
+            
+        }
+        static int battLoss(int batterie)
+        {
+            batterie -= 2;
+            return batterie;
+        }
+        static void death(int posX, int posY)
+        {
             //Last position + death
-            Console.Clear() ;
             Console.SetCursorPosition(posX, posY);
             Console.ForegroundColor = ConsoleColor.Red;
             Console.Write("x---x");
             Console.ForegroundColor = ConsoleColor.White;
-            Console.Read();
-
         }
-    }
+}
 }
