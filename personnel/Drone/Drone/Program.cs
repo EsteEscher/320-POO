9efb2668130ef7553b66dba8f2aea4463be3f0da
esteban-escher <pi53hgb@eduvaud.ch>
2026-09-04 12:04:51 +0200

    refactor(code) refactor du code de l'exercice drone

--- personnel/Drone/Drone/Program.cs
+++ personnel/Drone/Drone/Program.cs
@@ -11,53 +11,20 @@ namespace Drone
     {
         static void Main(string[] args)
         {
-            //Premier clear et appariton de départ drone
-            Console.Clear();
-            Console.Write("x-0-x");
-
             //Variable
-            int posX = 0;
-            int posY = 10;
-            int posX1 = 5;
-            int posY1 = 5;
-            int batterie = 50;
-            int batterie2 = 50;
+            Drone drone1 = new Drone(10, 6, 76);
+            Drone drone2 = new Drone(13, 4, 66);
 
-            //Boucle pour le faire avancer
-            while (batterie >= 2 || batterie2 >= 2)
+
+            while (drone1.Batterie > 0 || drone2.Batterie > 0)
             {
-                posX++;
-                posX1++;
-                batterie = battLoss(batterie);
-                drawDrone(posX, posY);
-                drawDrone(posX1, posY1);
-                Thread.Sleep(150);
                 Console.Clear();
+                drone1.changeState();
+                drone1.draw();
+                drone2.changeState();
+                drone2.draw();
+                Thread.Sleep(100);
             }
-            Console.Clear();
-            death(posX, posY);
-            death(posX1, posY1);
-            Console.Read();
-
         }
-        static void drawDrone(int posX, int posY)
-        {
-            Console.SetCursorPosition(posX, posY);
-            Console.Write("x-0-x");
-            
-        }
-        static int battLoss(int batterie)
-        {
-            batterie -= 2;
-            return batterie;
-        }
-        static void death(int posX, int posY)
-        {
-            //Last position + death
-            Console.SetCursorPosition(posX, posY);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.Write("x---x");
-            Console.ForegroundColor = ConsoleColor.White;
-        }
-}
+    }
 }
--- personnel/Drone/Drone/drone.cs
+++ personnel/Drone/Drone/drone.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace Drone
 {
-    internal class drone
+    public class Drone
     {
+        //propreties
+        private int _posx;
+        private int _posy;
+        private int _batterie;
+
+        public int Batterie { get => _batterie; set => _batterie = value; }
+
+        public Drone(int posx, int posy, int batterie)
+        {
+            this._posx = posx;
+            this._posy = posy;
+            this._batterie = batterie;
+        }
+        public Drone()
+        {
+            this._posx = 0;
+            this._posy = 0;
+            this._batterie = 66;
+        }
+        public void draw()
+        {
+            Console.SetCursorPosition(_posx, _posy);
+            Console.Write("x-0-x");
+        }
+        public void changeState()
+        {
+            if (_batterie < 0) return;
+            _posx++;
+            _batterie -= 2;
+        }
     }
 }
