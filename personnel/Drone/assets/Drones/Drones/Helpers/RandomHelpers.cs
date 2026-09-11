c2f460d61f515aa46785ad715d343353bf00130f
esteban-escher <pi53hgb@eduvaud.ch>
2026-09-11 11:08:03 +0200

    Feat(code) ajout du calculateur de nb aléatoire

---
+++ personnel/Drone/assets/Drones/Drones/Helpers/RandomHelpers.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drones.Helpers
+{
+    internal static class RandomHelpers
+    {
+        private static Random _alea = new Random();
+        public static int Generating(int max)
+        {
+            return _alea.Next(max);
+        }
+    }
+}
