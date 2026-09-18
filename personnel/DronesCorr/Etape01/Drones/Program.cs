301a995aa3313158667a14f6b62e79ed50116206
EsteEscher <escher.este@gmail.com>
2026-09-18 12:03:30 +0200

    feat(Code) Les drones changent maintenant d'objectif après avoir trouver leur premier objectif

---
+++ personnel/DronesCorr/Etape01/Drones/Program.cs
@@ -0,0 +1,25 @@
+namespace Drones
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+
+            // Cr�ation de la flotte de drones
+            List<Drone> fleet= new List<Drone>();
+            fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT / 2, "Joe"));
+            for(int i = 0; i < 1000; i++)
+                fleet.Add(new Drone(Config.AIRSPACE_WIDTH / 2, Config.AIRSPACE_HEIGHT / 2, "Le vatican" + i));
+
+            // D�marrage
+            Application.Run(new AirSpace(fleet));
+        }
+    }
+}
