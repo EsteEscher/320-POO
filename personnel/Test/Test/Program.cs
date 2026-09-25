b826128d4dcc3f4da14f4a065facfda7ed885c6b
EsteEscher <escher.este@gmail.com>
2026-09-25 11:18:59 +0200

    feat(test)Test des tests fait pour les exos

---
+++ personnel/Test/Test/Program.cs
@@ -0,0 +1,13 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ReadKey();
+
+
+public class MyMath
+{
+    public static int Somme(int a, int b)
+    { 
+        return a + b;
+    }
+}
---
+++ personnel/Test/test_my_test/Test1.cs
@@ -0,0 +1,25 @@
+﻿
+namespace test_my_test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestThatSumOfTenAndTenIsTwenty()
+        {
+            //arrange
+            int x = 10;
+            int y = 10;
+            int z = -15;
+
+            //act
+            int res = MyMath.Somme(x, y);
+            int res2 = MyMath.Somme(x, z);
+
+            //assert
+            Assert.AreEqual(20, res);
+            Assert.AreEqual(-5, res2);
+
+        }
+    }
+}
